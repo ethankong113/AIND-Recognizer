b298ce3cdd1987a7c982d3094dee9a8b27823d84
ethankong113 <kongwan113@gmail.com>
2018-02-14 09:15:53 -0800

    first update

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -68,16 +68,42 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
-    def select(self):
-        """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
+    """
+    Sources:
+    [2] https://stats.stackexchange.com/questions/79955/scikit-learn-gaussianhmm-decode-vs-score
+    """
 
-        :return: GaussianHMM object
+    def calc_num_of_params(self, num_of_states, num_of_dp):
         """
+        number of params is also number of free params
+        Sources:
+        https://discussions.udacity.com/t/understanding-better-model-selection/232987/9
+        """
+        return (num_of_states ** 2) + (2 * num_of_states * num_of_dp) - 1
+
+
+    def calc_bic_score(self, log_likelihood, num_of_params, log_num_of_dp):
+        return (-2 * log_likelihood) + (num_of_params * log_num_of_dp)
+
+    def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        models = []
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                hmm_model = self.base_model(num_states)
+                log_likelihood = hmm_model.score(self.X, self.lengths)
+                num_of_dp = sum(self.lengths)
+                log_num_of_dp = np.log(num_of_dp)
+                num_of_params = self.calc_num_of_params(num_states, num_of_dp)
+                bic_score = self.calc_bic_score(log_likelihood, num_of_params, log_num_of_dp)
+                models.append((bic_score, hmm_model))
+            except:
+                pass
+        if not len(models):
+            return None
+
+        return min(models, key=lambda model: model[0])[1]
 
 
 class SelectorDIC(ModelSelector):
@@ -90,11 +116,32 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+    def calc_dic_score(self, log_likelihood_this, hmm_model, other_words):
+        avg_log_likelihood_others =  np.mean([hmm_model.score(word[0], word[1]) for word in other_words])
+        return log_likelihood_this - avg_log_likelihood_others
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        other_words = []
+        models = []
+
+        [other_words.append(self.hwords[word]) for word in self.words if word != self.this_word]
+
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                hmm_model = self.base_model(num_states)
+                log_likelihood_this = hmm_model.score(self.X, self.lengths)
+                dic_score = self.calc_dic_score(log_likelihood_this, hmm_model, other_words)
+                models.append((dic_score, hmm_model))
+            except:
+                pass
+
+        if not len(models):
+            return None
+
+        return max(models, key=lambda model: model[0])[1]
+
 
 
 class SelectorCV(ModelSelector):
@@ -104,6 +151,28 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        kf = KFold(3)
+        models = []
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                if len(self.sequences) > 2:
+                    cvs_scores = []
+                    hmm_model = self.base_model(num_states)
+                    for train_index, test_index in kf.split(self.sequences):
+                        X_test, lengths_test = combine_sequences(test_index, self.sequences)
+                        log_likelihood = hmm_model.score(X_test, lengths_test)
+                        cvs_scores.append(log_likelihood)
+                    avg_score = np.mean(cvs_scores)
+                    models.append((avg_score, hmm_model))
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+                else:
+                    hmm_model = self.base_model(num_states)
+                    log_likelihood = hmm_model.score(X, lengths)
+                    models.append((log_likelihood, hmm_model))
+            except:
+                pass
+
+        if not len(models):
+            return self.base_model(self.min_n_components)
+
+        return max(models, key=lambda model: model[0])[1]
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,6 +1,13 @@
 import warnings
 from asl_data import SinglesData
 
+def get_best_word(log_likelihoods):
+    best_word = None
+    best_score = 0
+    for word, score in log_likelihoods.items():
+        if best_word == None or score > best_score:
+            best_word, best_score = word, score
+    return best_word
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
@@ -20,6 +27,20 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for word_id, val in test_set.get_all_Xlengths().items():
+        current_sequence, current_lengths = test_set.get_item_Xlengths(word_id)
+        log_likelihoods = {}
+        for word, model in models.items():
+            try:
+                LogLvalue = model.score(current_sequence, current_lengths)
+                log_likelihoods[word] = LogLvalue
+            except:
+                log_likelihoods[word] = float("-inf")
+                continue
+        probabilities.append(log_likelihoods)
+        guesses.append(get_best_word(log_likelihoods))
+    return probabilities, guesses
+
+
+
+    # for id in range(0, len(test_set.get_all_Xlengths()))
